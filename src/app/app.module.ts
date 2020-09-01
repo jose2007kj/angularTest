b64364771518995b9a9546266ed5e5433fe4e687
jose2007kj <jose2007kj@gmail.com>
2020-09-01 15:48:18 +0530

    api fetch for table data and adding new element done installed angular material for using material table its all working fine need to add comments, check alignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,13 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild } from '@angular/core'; 
+import {HttpClient} from '@angular/common/http'; 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatTable} from '@angular/material/table';
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
   styleUrls: ['./table-list.component.css']
 })
+
+
 export class TableListComponent implements OnInit {
+  li:any; 
+  lis=[]; 
+  registerForm: FormGroup;
+  submitted = false;
+  @ViewChild(MatTable) table: MatTable<any>;
+  showform: boolean =false;
+  displayedColumns: string[] = ['name', 'position', 'office', 'salary'];
+  constructor(private http : HttpClient, private formBuilder: FormBuilder){ 
+      } 
+  
+  ngOnInit(): void { 
+     this.registerForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            position: ['', Validators.required],
+            office: ['', Validators.required],
+            salary: ['', Validators.required]
+        });
+    this.http.get('http://www.mocky.io/v2/5ea172973100002d001eeada') 
+    .subscribe(Response => { 
+  
+      // If response comes hideloader() function is called 
+      // to hide that loader  
+      if(Response){   
+        hideloader(); 
+      } 
+      console.log(Response) 
+      this.li=Response; 
+      this.lis=this.li.list; 
+    }); 
+    function hideloader(){ 
+      document.getElementById('loading').style.display = 'none';} 
+    }
+    get f() { return this.registerForm.controls; }
 
-  constructor() { }
+    onSubmit() {
+        this.submitted = true;
 
-  ngOnInit(): void {
-  }
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
 
-}
+            return;
+        }
+
+        // display form values on success
+        this.lis.push(this.registerForm.value)
+            console.log(this.lis);this.table.renderRows()
+            this.showform =false
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.registerForm.reset();
+    }
+    showForm(){
+      this.showform=true
+    }
+
+
+    } 
+
+  
